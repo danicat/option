eedd97ddae62a7776ec5895e8916f00197e7c195
Daniela Petruzalek <daniela.petruzalek@gmail.com>
2023-08-17 09:36:05 +0100

    add lift3 (#5)

--- lift.go
+++ lift.go
@@ -23,3 +23,14 @@ func Lift2[T1, T2, U any](f PartialFunction2[T1, T2, U]) Function2[T1, T2, U] {
 		return New(res)
 	}
 }
+
+func Lift3[T1, T2, T3, U any](f PartialFunction3[T1, T2, T3, U]) Function3[T1, T2, T3, U] {
+	return func(v1 T1, v2 T2, v3 T3) Option[U] {
+		res, err := f(v1, v2, v3)
+		if err != nil {
+			return Option[U]{}
+		}
+
+		return New(res)
+	}
+}
