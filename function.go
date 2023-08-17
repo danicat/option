170b06387c6e8e10d27a5a11456f86d17c077ec1
Daniela Petruzalek <daniela.petruzalek@gmail.com>
2023-08-17 23:38:36 +0100

    add lift4 and lift5

--- function.go
+++ function.go
@@ -3,7 +3,19 @@ package option
 type PartialFunction1[T, R any] func(T) (R, error)
 type PartialFunction2[T1, T2, R any] func(T1, T2) (R, error)
 type PartialFunction3[T1, T2, T3, R any] func(T1, T2, T3) (R, error)
+type PartialFunction4[T1, T2, T3, T4, R any] func(T1, T2, T3, T4) (R, error)
+type PartialFunction5[T1, T2, T3, T4, T5, R any] func(T1, T2, T3, T4, T5) (R, error)
+type PartialFunction6[T1, T2, T3, T4, T5, T6, R any] func(T1, T2, T3, T4, T5, T6) (R, error)
+type PartialFunction7[T1, T2, T3, T4, T5, T6, T7, R any] func(T1, T2, T3, T4, T5, T6, T7) (R, error)
+type PartialFunction8[T1, T2, T3, T4, T5, T6, T7, T8, R any] func(T1, T2, T3, T4, T5, T6, T7, T8) (R, error)
+type PartialFunction9[T1, T2, T3, T4, T5, T6, T7, T8, T9, R any] func(T1, T2, T3, T4, T5, T6, T7, T8, T9) (R, error)
 
 type Function1[T, R any] func(T) Option[R]
 type Function2[T1, T2, R any] func(T1, T2) Option[R]
 type Function3[T1, T2, T3, R any] func(T1, T2, T3) Option[R]
+type Function4[T1, T2, T3, T4, R any] func(T1, T2, T3, T4) Option[R]
+type Function5[T1, T2, T3, T4, T5, R any] func(T1, T2, T3, T4, T5) Option[R]
+type Function6[T1, T2, T3, T4, T5, T6, R any] func(T1, T2, T3, T4, T5, T6) Option[R]
+type Function7[T1, T2, T3, T4, T5, T6, T7, R any] func(T1, T2, T3, T4, T5, T6, T7) Option[R]
+type Function8[T1, T2, T3, T4, T5, T6, T7, T8, R any] func(T1, T2, T3, T4, T5, T6, T7, T8) Option[R]
+type Function9[T1, T2, T3, T4, T5, T6, T7, T8, T9, R any] func(T1, T2, T3, T4, T5, T6, T7, T8, T9) Option[R]
--- lift.go
+++ lift.go
@@ -1,7 +1,7 @@
 package option
 
-// Lift extends the domain of a partial function to the full domain by
-// returning an Option instead of a value-error pair
+// Lift extends the domain of a PartialFunction1 to the full domain by
+// converting it to a Function1 returning an Option[U].
 func Lift[T, U any](f PartialFunction1[T, U]) Function1[T, U] {
 	return func(v T) Option[U] {
 		res, err := f(v)
@@ -13,6 +13,8 @@ func Lift[T, U any](f PartialFunction1[T, U]) Function1[T, U] {
 	}
 }
 
+// Lift2 extends the domain of a PartialFunction2 to the full domain by
+// converting it to a Function2 returning an Option[U].
 func Lift2[T1, T2, U any](f PartialFunction2[T1, T2, U]) Function2[T1, T2, U] {
 	return func(v1 T1, v2 T2) Option[U] {
 		res, err := f(v1, v2)
@@ -24,6 +26,8 @@ func Lift2[T1, T2, U any](f PartialFunction2[T1, T2, U]) Function2[T1, T2, U] {
 	}
 }
 
+// Lift3 extends the domain of a PartialFunction3 to the full domain by
+// converting it to a Function3 returning an Option[U].
 func Lift3[T1, T2, T3, U any](f PartialFunction3[T1, T2, T3, U]) Function3[T1, T2, T3, U] {
 	return func(v1 T1, v2 T2, v3 T3) Option[U] {
 		res, err := f(v1, v2, v3)
@@ -34,3 +38,31 @@ func Lift3[T1, T2, T3, U any](f PartialFunction3[T1, T2, T3, U]) Function3[T1, T
 		return New(res)
 	}
 }
+
+// Lift4 extends the domain of a PartialFunction4 to the full domain by
+// converting it to a Function4 returning an Option[U].
+func Lift4[T1, T2, T3, T4, U any](f PartialFunction4[T1, T2, T3, T4, U]) Function4[T1, T2, T3, T4, U] {
+	return func(v1 T1, v2 T2, v3 T3, v4 T4) Option[U] {
+		res, err := f(v1, v2, v3, v4)
+		if err != nil {
+			return Option[U]{}
+		}
+
+		return New(res)
+	}
+}
+
+// Lift5 extends the domain of a PartialFunction4 to the full domain by
+// converting it to a Function4 returning an Option[U].
+func Lift5[T1, T2, T3, T4, T5, U any](f PartialFunction5[T1, T2, T3, T4, T5, U]) Function5[T1, T2, T3, T4, T5, U] {
+	return func(v1 T1, v2 T2, v3 T3, v4 T4, v5 T5) Option[U] {
+		res, err := f(v1, v2, v3, v4, v5)
+		if err != nil {
+			return Option[U]{}
+		}
+
+		return New(res)
+	}
+}
+
+// TODO: implement Lift6 to Lift9
